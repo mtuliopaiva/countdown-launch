7b9209990f719378d0b19f9a1ac83e1b3ee10ca2
Marco Túlio Sá de Paiva <tulio.paiva10@gmail.com>
2022-08-31 17:55:28 -0300

    Ajustando funcionalidades, segundos, minutos e horas funcionando, proximo passo: ajustar dias e final do contador

--- main.js
+++ main.js
@@ -7,22 +7,43 @@ contaSegundo();
 
 
     function contaSegundo() {
+        var t = segundos;
         var myInterval = setInterval(function(){
+            segundos--;
             document.querySelector(".segundos__contador").textContent = segundos;
-        segundos--;
-
         },1000);
 
         setTimeout(function() {
             clearInterval(myInterval);
             segundos = 59;
             contaSegundo();
-            contaMinuto();
-        },60000)
+            relogio();
+            
+
+        },t*1000)
     }
-    function contaMinuto() {
-        minutos--;
-        console.log(minutos);
+    function relogio() {
+        if(minutos != '0'){
+            minutos--;
+            document.querySelector(".minutos__contador").textContent = minutos;
+        }
+        else{
+            minutos = 59;
+            horas--;
+            document.querySelector(".minutos__contador").textContent = minutos;
+            document.querySelector(".horas__contador").textContent = horas;
+            if(horas =='0'){
+                horas = 23;
+                document.querySelector(".horas__contador").textContent = horas;
+                if(dias!='0'){
+                    dias--;
+                    document.querySelector(".dias__contador").textContent = dias;
+                }
+                if(dias =='0'){
+                    document.querySelector(".dias__contador").textContent = dias;
+                }
+            }
+        }
     }
 
 
